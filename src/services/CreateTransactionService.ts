f16243eeb5b1063329bc86d7cea6c3ac0bc8072b
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-01-16 16:51:41 -0300

    Implementing transaction route and its rules

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    let transactionCategory;
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError(' You do not have enough balance');
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      transactionCategory = newCategory;
+    } else {
+      transactionCategory = checkCategoryExists;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
