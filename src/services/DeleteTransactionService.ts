f024248a7e0e9cca8448a0ffb884fd0b30553c00
Vinicius Ribeiro <vinicius.ribeiro@contraprova.com.br>
2021-01-17 18:14:31 -0300

    Fixing delete route

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,20 +1,19 @@
 import { getCustomRepository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import AppError from '../errors/AppError';
 
-interface Request {
-  id: string;
-}
-
 class DeleteTransactionService {
-  public async execute({ id }: Request): Promise<void> {
+  public async execute(id: string): Promise<void> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
 
     const transaction = await transactionsRepository.findOne(id);
 
     if (!transaction) {
-      throw new AppError('Transaction does not exists!');
+      throw new AppError('Transaction does not exist.');
     }
 
     await transactionsRepository.remove(transaction);
